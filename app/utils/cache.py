04ad905ff415ae8fb6f6a091dedc858e63405ae9
ArtemLevin <hooch19@mail.ru>
2025-07-19 19:17:43 +0300

    refactor: fix cache

--- app/utils/cache.py
+++ app/utils/cache.py
@@ -10,5 +10,14 @@ class RedisPool:
 
     async def get_client(self):
         if not self._client:
-            self._client = redis.from_url(str(settings.redis_url), max_connections=settings.redis_max_connections)
+            try:
+                self._client = redis.from_url(
+                    str(settings.redis_url),
+                    max_connections=settings.redis_max_connections,
+                    socket_timeout=5
+                )
+                await self._client.ping()
+            except Exception as e:
+                logger.error("Failed to connect to Redis", error=str(e))
+                raise
         return self._client
