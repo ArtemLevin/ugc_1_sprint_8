07970bbb1a091ad38b891a6f4d2d4bca40ddcb6f
ArtemLevin <hooch19@mail.ru>
2025-07-19 19:18:55 +0300

    refactor: fix logger

--- app/core/logger.py
+++ app/core/logger.py
@@ -3,4 +3,6 @@ from opentelemetry import trace
 
 def get_logger(name: str):
     tracer = trace.get_tracer(name)
-    return structlog.get_logger(name).bind(trace_id=str(tracer.get_current_span().context.trace_id))
+    return structlog.get_logger(name).bind(
+        trace_id=str(tracer.get_current_span().context.trace_id) if tracer.get_current_span() else "unknown"
+    )
