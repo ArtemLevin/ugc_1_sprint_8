a3fe5036dfae2dbb5ab6aa07912fd773472d7070
ArtemLevin <hooch19@mail.ru>
2025-07-19 23:54:28 +0300

    refactor: fix lua script

--- app/utils/lua/rate_limiting.lua
+++ app/utils/lua/rate_limiting.lua
@@ -4,13 +4,16 @@ local capacity = tonumber(ARGV[2])
 local now = tonumber(ARGV[3])
 local window = tonumber(ARGV[4])
 
-redis.call('ZREMRANGEBYSCORE', key, 0, now - window)
-
 local current_tokens = redis.call('ZCARD', key)
 
-if current_tokens < capacity then
-    redis.call('ZADD', key, now, now)
-    return 1
-else
+if current_tokens >= capacity then
     return 0
 end
+
+local oldest = redis.call('ZRANGE', key, 0, 0, 'WITHSCORES')
+if oldest and #oldest > 0 and now - oldest[2] > window then
+    redis.call('ZREMRANGEBYSCORE', key, 0, now - window)
+end
+
+redis.call('ZADD', key, now, now)
+return 1
